0dd815023374b3d4deba41ae08aee26412c50d1a
Scott Schmidt <sjschmidt44@gmail.com>
2015-04-28 11:17:07 -0700

    Fix Edit/Update/Delete button functionality.

--- cf_django_app/static/js/app.js
+++ cf_django_app/static/js/app.js
@@ -1,34 +1,37 @@
-function toggleEditable(userid){
-    var EditButton = document.getElementById("EditButton_"+userid);
-    if(EditButton.textContent == "Edit"){
-      //User wants to edit or delete this record, so enable the fields
-        setFieldsDisabled(userid, false);
-    //...and set the Edit button to now be a Cancel button
-        EditButton.textContent = "Cancel";
-    }
-    else{
-        //User cancelled editing this record, so reset fields to previous values
-        var FirstNameField = document.getElementById("FirstName_"+userid);
-        var LastNameField = document.getElementById("LastName_"+userid);
-        var EmailField = document.getElementById("Email_"+userid);
-        FirstNameField.value = document.getElementById("OriginalFirstName_"+userid).value;
-        LastNameField.value = document.getElementById("OriginalLastName_"+userid).value;
-    //...and reset the record UI to read-only mode
-        EmailField.value = document.getElementById("OriginalEmail_"+userid).value;
-        setFieldsDisabled(userid, true);
-        EditButton.textContent = "Edit";
-    }
+function toggleEditable(userid) {
+  var editButton = document.getElementById("EditButton_" + userid);
+
+  if(editButton.textContent == "Edit") {
+    setFieldsDisabled(userid, false);
+    editButton.textContent = "Cancel";
+  }else{
+    var firstNameField = document.getElementById("FirstName_" + userid);
+    var lastNameField = document.getElementById("LastName_" + userid);
+    var emailField = document.getElementById("Email_" + userid);
+
+    firstNameField.value = document.getElementById("OriginalFirstName_" + userid).value;
+    lastNameField.value = document.getElementById("OriginalLastName_" + userid).value;
+    emailField.value = document.getElementById("OriginalEmail_" + userid).value;
+    setFieldsDisabled(userid, true);
+    editButton.textContent = "Edit";
+  }
 }
-//used to bulk set a given record's fields
+
 function setFieldsDisabled(userid, value){
-    var FirstNameField = document.getElementById("FirstName_"+userid);
-    var LastNameField = document.getElementById("LastName_"+userid);
-    var EmailField = document.getElementById("Email_"+userid);
-    var updateButton = document.getElementById("Update_"+userid);
-    var deleteButton = document.getElementById("Delete_"+userid);
-    FirstNameField.disabled = value;
-    LastNameField.disabled = value;
-    EmailField.disabled = value;
-    updateButton.disabled = value;
-    deleteButton.disabled = value;
+  var firstNameField = document.getElementById("FirstName_" + userid);
+  var lastNameField = document.getElementById("LastName_" + userid);
+  var emailField = document.getElementById("Email_" + userid);
+  var updateButton = document.getElementById("Update_" + userid);
+  var deleteButton = document.getElementById("Delete_" + userid);
+  firstNameField.disabled = value;
+  lastNameField.disabled = value;
+  emailField.disabled = value;
+
+  if (value == false) {
+    updateButton.style.display = '';
+    deleteButton.style.display = '';
+  } else {
+    updateButton.style.display = 'none';
+    deleteButton.style.display = 'none';
+  }
 }
